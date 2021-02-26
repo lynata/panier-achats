bde630d8defc355056192c00f12f1a532c25fb03
Lyna <lyly8888@hotmail.com>
2021-02-26 09:38:05 -0500

    fin cours 10

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -5,7 +5,7 @@ import instanceFirebase from 'firebase/app';
 import "firebase/firestore";
 
 // code pour integre firebase temporairement
-//configurtion du projet fb
+//etape 1 configurtion du projet fb
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDBkHd3rUW8dZ5D07SUpNkPG7yMxRuWbdM",
@@ -17,16 +17,41 @@ const firebaseConfig = {
     measurementId: "G-6LD8NJ7390"
 };
 
-//obtenir une instance de l'api firebase
+//etape 2 obtenir une instance de l'api firebase
 if(!instanceFirebase.apps.length) {
     instanceFirebase.initializeApp(firebaseConfig);
 }
 
-//obtenir une instance de la bd firestore
-const bd = instanceFirebase.firestore;
+//etape 3 obtenir une instance de la bd firestore
+const bd = instanceFirebase.firestore();
 
-//AJAX et Promesse et asynchrone
-// bd.collection("produits").get();
+//etape 4 exemple decriture et de lecture de donnees
+
+//exemple 1 : ajouter tout les produits du fichiers JSON dans la collection "produits" sur firestore
+// tabProduits.forEach(
+//     prd => bd.collection("produits").add(prd).then(
+//         redDoc => console.log("Le document est ajoute avec identifiant : ", redDoc.id)
+//     )
+// );
+
+// exemple 2 chercher des documents dans une collection
+// bd.collection("produits").where("prix", ">", 15).get().then(
+//     reponse => reponse.forEach(
+//         article => console.log("Produits plus cher que 15$", article.data())
+//     )
+// )
+
+//exemple 3 : observer une collection
+// pour les chats, games, jeux
+bd.collection("produits").onSnapshot(
+    snapshot => snapshot.docChanges().forEach(
+        changement => {
+            if(changement.type == "added"){
+                console.log(changement.doc.data());
+            }
+        }
+    )
+)
 
 function ListeProduits(props) {
     console.log("Panier dans ListeProduits", props.etatPanier);
